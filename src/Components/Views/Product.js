cebc88e15e1fc1c16345d5a33502c6e7055b2dfa
ToeWaiPhyo <toewaiphyo@ucstgi.edu.mm>
2022-03-01 11:38:32 +0630

    testing react

---
+++ src/Components/Views/Product.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function Product() {
+    const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/3";
+    const [product, setproduct] = useState(null);
+    const content = null;
+    useEffect(() => {
+        axios.get(url).then(response => {
+            setproduct(response.data)
+        })
+    }, [url])
+
+    if (product) {
+        content =
+            <div>
+                <h1 className="text-2xl font-bold mb-3">
+                    {product.name}
+                </h1>
+                <div>
+                    <img src={product.image[0].imageUrl}
+                        alt={product.name} />
+                </div>
+                
+            </div>
+       </div >
+    }
+    return (
+        { content }
+    )
+
+}
+export default Product;
