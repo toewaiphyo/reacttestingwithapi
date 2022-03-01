55feda5c53f3e2f32b4ef348fe3e3541aaff24f9
ToeWaiPhyo <toewaiphyo@ucstgi.edu.mm>
2022-03-01 12:17:06 +0630

    adding loading to fetch product data

--- src/Components/Views/Product.js
+++ src/Components/Views/Product.js
@@ -1,32 +1,57 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import Loader from '../loader'
 
 function Product() {
-    const url = "https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/3";
-    const [product, setproduct] = useState(null);
-    const content = null;
+    const { id } = useParams();
+    const url = `https://5e9623dc5b19f10016b5e31f.mockapi.io/api/v1/products/${id}`;
+    let content = null;
+    const [product, setproduct] = useState({
+        loading: false,
+        data: null
+    }
+    );
     useEffect(() => {
+        setproduct({ 
+            loading: true, 
+            data: null 
+        })
         axios.get(url).then(response => {
-            setproduct(response.data)
+            setproduct({ 
+                loading: false, 
+                data: response.data 
+            })
         })
     }, [url])
+    if(product.loading){
+        content=<Loader/>
+    }
+   
 
-    if (product) {
+    if (product.data) {
         content =
             <div>
                 <h1 className="text-2xl font-bold mb-3">
-                    {product.name}
+                    {product.data.name}
                 </h1>
-                <div>
-                    <img src={product.image[0].imageUrl}
-                        alt={product.name} />
+                <div className="p-1">
+                    <img src={product.data.images[0].imageUrl}
+                        alt={product.data.name} />
                 </div>
-                
+                <div className="font-bold text-xl mb-3">
+                    $ {product.data.price}
+                </div>
+                <div>
+                    {product.data.description}
+                </div>
+
             </div>
-       
     }
     return (
-        { content }
+        <div>
+            {content}
+        </div>
     )
 
 }
